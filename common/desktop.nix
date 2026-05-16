567157194ac04643b3451f36031864dd97d0d699
Quentin <quentin.bau30@gmail.com>
2026-05-16 19:53:08 +0200

    Wallpaper settings

--- common/desktop.nix
+++ common/desktop.nix
@@ -1,10 +1,34 @@
 { config, pkgs, ... }:
 
+let
+  tranquilityWallpaper = ../assets/wallpaper.png;
+  initWallpaperScript = pkgs.writeShellScriptBin "init-tranquility-wallpaper" ''
+    FLAG_FILE="$HOME/.config/tranquility_wallpaper_initialized"
+
+    if [ ! -f "$FLAG_FILE" ]; then
+      sleep 5
+      ${pkgs.kdePackages.plasma-workspace}/bin/plasma-apply-wallpaperimage ${tranquilityWallpaper}
+      touch "$FLAG_FILE"
+    fi
+  '';
+in
+
 {
   services.xserver.enable = true;
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  environment.systemPackages = [ initWallpaperScript ];
+  environment.etc."xdg/autostart/init-tranquility-wallpaper.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Exec=${initWallpaperScript}/bin/init-tranquility-wallpaper
+    Hidden=false
+    NoDisplay=false
+    Name=Initialisation du fond d'écran TranquilityOS
+    Comment=Applique le fond d'écran par défaut à la création du profil
+  '';
 
   console.keyMap = "fr";
   services.xserver.xkb = {
