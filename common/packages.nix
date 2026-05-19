6c8b34708ee0edcf9f57a7da6e93e4851bd3838d
Quentinnnnn11 <quentin.bau30@gmail.com>
2026-05-19 15:55:35 +0200

    config LaSuite

--- common/packages.nix
+++ common/packages.nix
@@ -23,41 +23,54 @@
     chromium
 
     #LaSuite
+    (writeTextDir "share/desktop-directories/lasuite.directory" ''
+      [Desktop Entry]
+      Type=Directory
+      Name=LaSuite
+      Icon=folder-favorites
+      Comment=Suite numérique de l'État
+    '')
     (makeDesktopItem {
       name = "lasuite-tchap";
       desktopName = "Tchap - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://www.tchap.gouv.fr";
       icon = "${../assets/tchap.svg}";
+      categories = [ "LaSuite" ];
     })
     (makeDesktopItem {
       name = "lasuite-visio";
       desktopName = "Visio - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/visio";
       icon = "${../assets/visio.svg}";
+      categories = [ "LaSuite" ];
     })
     (makeDesktopItem {
       name = "lasuite-docs";
       desktopName = "Docs - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/docs";
       icon = "${../assets/docs.svg}";
+      categories = [ "LaSuite" ];
     })
     (makeDesktopItem {
       name = "lasuite-grist";
       desktopName = "Grist - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/grist";
       icon = "${../assets/grist.svg}";
+      categories = [ "LaSuite" ];
     })
     (makeDesktopItem {
       name = "lasuite-fichiers";
       desktopName = "Fichiers - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/fichiers";
       icon = "${../assets/fichiers.svg}";
+      categories = [ "LaSuite" ];
     })
     (makeDesktopItem {
       name = "francetransfert-grist";
       desktopName = "FranceTransfert - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://francetransfert.numerique.gouv.fr/upload";
       icon = "${../assets/francetransfert.svg}";
+      categories = [ "LaSuite" ];
     })
   ];
 
