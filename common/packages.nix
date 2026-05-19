c8f13074ea8f83dbfd2972510b86ebb9a99fe798
Quentinnnnn11 <88734974+Quentinnnnn11@users.noreply.github.com>
2026-05-19 15:27:09 +0200

    Add LaSuite

--- common/packages.nix
+++ common/packages.nix
@@ -9,22 +9,56 @@
     wget
     curl
     htop
-    libreoffice-qt
-    hunspell
-    hunspellDicts.fr-moderne
     thunderbird
     firefox
-    remmina
-    neovim
-    signal-desktop
+    remmina #RDP
     p7zip
     unzip
     unrar
     vlc
     obsidian
     libqalculate
-    noisetorch
+    noisetorch #reduction de bruit pour micro
     gnome-power-manager
+    chromium
+
+    #LaSuite
+    (makeDesktopItem {
+      name = "lasuite-tchap";
+      desktopName = "Tchap - LaSuite";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://www.tchap.gouv.fr";
+      icon = "${./assets/tchap.svg}";
+    })
+    (makeDesktopItem {
+      name = "lasuite-visio";
+      desktopName = "Visio - LaSuite";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/visio";
+      icon = "${./assets/visio.svg}";
+    })
+    (makeDesktopItem {
+      name = "lasuite-docs";
+      desktopName = "Docs - LaSuite";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/docs";
+      icon = "${./assets/docs.svg}";
+    })
+    (makeDesktopItem {
+      name = "lasuite-grist";
+      desktopName = "Grist - LaSuite";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/grist";
+      icon = "${./assets/grist.svg}";
+    })
+    (makeDesktopItem {
+      name = "lasuite-fichiers";
+      desktopName = "Fichiers - LaSuite";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/fichiers";
+      icon = "${./assets/fichiers.svg}";
+    })
+    (makeDesktopItem {
+      name = "francetransfert-grist";
+      desktopName = "FranceTransfert - LaSuite";
+      exec = "${pkgs.chromium}/bin/chromium --app=https://francetransfert.numerique.gouv.fr/upload";
+      icon = "${./assets/francetransfert.svg}";
+    })
   ];
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
