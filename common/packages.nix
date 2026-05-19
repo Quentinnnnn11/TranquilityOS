fa0d295870f5f1c7ffc56f5e20bebb73e8153de8
Quentinnnnn11 <quentin.bau30@gmail.com>
2026-05-19 15:34:19 +0200

    add LaSuite icons

--- common/packages.nix
+++ common/packages.nix
@@ -27,37 +27,37 @@
       name = "lasuite-tchap";
       desktopName = "Tchap - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://www.tchap.gouv.fr";
-      icon = "${./assets/tchap.svg}";
+      icon = "${../assets/tchap.svg}";
     })
     (makeDesktopItem {
       name = "lasuite-visio";
       desktopName = "Visio - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/visio";
-      icon = "${./assets/visio.svg}";
+      icon = "${../assets/visio.svg}";
     })
     (makeDesktopItem {
       name = "lasuite-docs";
       desktopName = "Docs - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/docs";
-      icon = "${./assets/docs.svg}";
+      icon = "${../assets/docs.svg}";
     })
     (makeDesktopItem {
       name = "lasuite-grist";
       desktopName = "Grist - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/grist";
-      icon = "${./assets/grist.svg}";
+      icon = "${../assets/grist.svg}";
     })
     (makeDesktopItem {
       name = "lasuite-fichiers";
       desktopName = "Fichiers - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://lasuite.numerique.gouv.fr/produits/fichiers";
-      icon = "${./assets/fichiers.svg}";
+      icon = "${../assets/fichiers.svg}";
     })
     (makeDesktopItem {
       name = "francetransfert-grist";
       desktopName = "FranceTransfert - LaSuite";
       exec = "${pkgs.chromium}/bin/chromium --app=https://francetransfert.numerique.gouv.fr/upload";
-      icon = "${./assets/francetransfert.svg}";
+      icon = "${../assets/francetransfert.svg}";
     })
   ];
 
