b7a894021694d004d0b87e5517fb0723d06ddda7
Quentinnnnn11 <quentin.bau30@gmail.com>
2026-05-21 23:02:23 +0200

    debug

--- common/security.nix
+++ common/security.nix
@@ -31,7 +31,6 @@
     "mce=0"
     "rng_core.default_quality=500"
     "mds=full"
-    "security=yama"
   ];
 
   # https://messervices.cyber.gouv.fr/documents-guides/fr_np_linux_configuration-v2.0.pdf
@@ -47,6 +46,6 @@
     "kernel.sysrq" = 0;
     "kernel.unprivileged_bpf_disabled" = 1;
     "kernel.panic_on_oops" = 1;
-    "kernel.yama.ptrace_scope" = 2;
+    "kernel.yama.ptrace_scope" = 2; #si utilisateurs dev, passer à 1
   };
 }
